94a2c678968828af22fba740c6d4d7fdd7a13249
amissahmary <mamissah2@byupathway.edu>
2026-01-14 22:51:53 +0000

    modified:   scripts/temples.js 	modified:   styles/temples-large.css 	modified:   temples.html

--- scripts/temples.js
+++ scripts/temples.js
@@ -8,10 +8,9 @@ short.innerHTML = `Last modified: <span class="highlight">${new Intl.DateTimeFor
 	}
 ).format(today)}</span>`;
 
-const hanButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
+const hanButton = document.querySelector('.hamBurger');
+const navigation = document.getElementById('nav');
 
-hanButton.addEventListener('click', () => {
+hanButton.addEventListener('click', function () {
     navigation.classList.toggle('open');
-    hanButton.classList.toggle('open');
 });
