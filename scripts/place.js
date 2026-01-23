019e2f07c1f4ab6662ea4db6858e1a3029019099
amissahmary <mamissah2@byupathway.edu>
2026-01-23 16:12:25 +0000

    modified:   place.html 	modified:   scripts/place.js

--- scripts/place.js
+++ scripts/place.js
@@ -20,9 +20,12 @@ function calculateWindChill(temp, speed){
     return (13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16)).toFixed(1);
 }
 
-let windChill = "N/A";
+let windChill = 0;
 if (temp <= 10 && windSpeed > 4.8) {
-    windChill = calculateWindChill(temp, windSpeed) + "°C";
+	windChill = calculateWindChill(temp, windSpeed);
 }
 
 document.getElementById("windChill").textcontent = windChill;
+
+
+
